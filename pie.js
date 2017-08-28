221d8c753f6fda83c82e1e1712e42610dbf1fb4c
Tyler Long <tyler4long@gmail.com>
2017-08-28 17:23:48 +0800

    Initial commit

---
+++ pie.js
@@ -0,0 +1,57 @@
+var fs = require('fs')
+var d3 = require('d3')
+var jsdom = require('jsdom/lib/old-api')
+
+const chartWidth = 500
+const chartHeight = 500
+
+var arc = d3.svg.arc()
+  .outerRadius(chartWidth / 2 - 10)
+  .innerRadius(0)
+
+var colours = ['#F00', '#000', '#000', '#000', '#000', '#000', '#000', '#000', '#000']
+
+module.exports = function (pieData, outputLocation) {
+  if (!pieData) pieData = [12, 31]
+  if (!outputLocation) outputLocation = 'demo.svg'
+
+  jsdom.env({
+    html: '',
+    features: { QuerySelector: true }, // you need query selector for D3 to work
+    done: function (errors, window) {
+      window.d3 = d3.select(window.document) // get d3 into the dom
+
+      // do yr normal d3 stuff
+      var svg = window.d3.select('body')
+        .append('div').attr('class', 'container') // make a container div to ease the saving process
+        .append('svg')
+        .attr({
+          xmlns: 'http://www.w3.org/2000/svg',
+          width: chartWidth,
+          height: chartHeight
+        })
+        .append('g')
+        .attr('transform', 'translate(' + chartWidth / 2 + ',' + chartWidth / 2 + ')')
+
+      svg.selectAll('.arc')
+        .data(d3.layout.pie()(pieData))
+        .enter()
+        .append('path')
+        .attr({
+          'class': 'arc',
+          'd': arc,
+          'fill': function (d, i) {
+            return colours[i]
+          },
+          'stroke': '#fff'
+        })
+
+      // write out the children of the container div
+      fs.writeFileSync(outputLocation, window.d3.select('.container').html()) // using sync to keep the code simple
+    }
+  })
+}
+
+if (require.main === module) {
+  module.exports()
+}
